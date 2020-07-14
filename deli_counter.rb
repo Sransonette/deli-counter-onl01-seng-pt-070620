f40d86f8e7ec58f0efeb60e5ceb87bcc957d4752
Sean Ransonette <sransonette@gmail.com>
2020-07-14 23:13:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(katz_deli)
       puts "The line is currently empty."
     else
       new_line = "The line is currently:"
-      katz_deli.each_with_index do |name[0], number|
-      new_line << "#{name} and #{number}" 
+      katz_deli.each_with_index do |name, number|
+      new_line << "#{name + 1} and #{number}" 
    end
    puts new_line
 end
